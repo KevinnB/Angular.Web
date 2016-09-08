873c657ae3c3139c80c6246fdd8d931bf85af704
KevinnB <kleach0@gmail.com>
2016-09-07 19:03:06 -0500

    Added basic project and stripped out most stuff

---
+++ Web/Web/Controllers/PartialController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+
+    public class PartialController : Controller
+    {
+        public ActionResult Card()
+        {
+            return Card();
+        }
+    }
+}
---
+++ Web/Web/gulpfile.js
@@ -0,0 +1,25 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+     .pipe(sourcemaps.init())
+     .pipe(sass().on('error', sass.logError))
+     .pipe(sourcemaps.write('./maps'))
+     .pipe(gulp.dest('./css/site.css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
